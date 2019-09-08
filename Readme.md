8b641e5c7395b7a8e4fc89704c23dc2a089d5bad
lleontan <tanl2@uw.edu>
2019-09-07 22:14:25 -0700

    Added readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# The personal website of lleontan
+## [Ayy, the link my dudes](https://lleontan-115b9.firebaseapp.com)
--- nodeApp.js
+++ nodeApp.js
@@ -1,3 +1,17 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+  apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appID: "app-id"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 var path = require('path');
 var http = require('http');
 var fs = require('fs');
